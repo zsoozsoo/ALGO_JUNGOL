93b5f5036c0e60fde48b905007ee1b0836cddc80
zsoozsoo <zsoozsoo@naver.com>
2021-03-21 03:30:57 +0900

    Q1863

---
+++ src/Q1863.java
@@ -0,0 +1,53 @@
+package ssafy_algo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q1863 {
+	
+	static int n,m;
+	static int[] parents;
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		parents = new int[n+1];
+		for (int i = 1; i <= n; i++) { //일단 자기자신을 배열에 하나씩 넣어주기
+			parents[i] = i;
+		}
+		
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			union(a,b);
+		}
+		
+		int cnt = 0;
+		for (int i = 1; i <= n; i++) {
+			if(i==parents[i]) cnt++;
+		}
+		
+		System.out.println(cnt);
+		
+	}
+	
+	private static boolean union(int a, int b) {
+		int aRoot = findSet(a);
+		int bRoot = findSet(b);
+		if(aRoot==bRoot) return false;
+		parents[bRoot] = aRoot;
+		return true;
+	}
+	
+	private static int findSet(int a) {
+		if(parents[a]==a) return a;
+		return parents[a] = findSet(parents[a]);
+	}
+}
